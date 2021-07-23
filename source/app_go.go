fa39a38fa0f2d7df3ecda3c2028293422143f558
Antosha <cmex1966@gmail.com>
2021-07-23 15:10:29 +0300

    Test

--- source/app_go.go
+++ source/app_go.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello!\nThis is Go App!\nMade with 💖 by Antosha")
+		c.String(http.StatusOK, "Hello!\nThis is Go App!\nMade with 💖 by Antosha\nИДИ РАБОТАЙ МАЛЕНЬКАЯ ВРЕДНАЯ КАРЯВКА!")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
