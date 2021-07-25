d98b7c52ad8fcdf7d4b9bb64f6e5d230b5411345
Antosha <cmex1966@gmail.com>
2021-07-25 18:12:40 +0300

    Added version page

--- source/app_go.go
+++ source/app_go.go
@@ -10,5 +10,10 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Wake up Neo!\nThis is Go App!\nMade with 💖 by Antosha")
 	})
+
+	r.GET("/version", func(c *gin.Context) {
+		c.String(http.StatusOK, "v2.0")
+	})
+	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
