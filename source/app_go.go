741875f6d695c89d16d6e9835e855cfbb59e437e
Antosha <cmex1966@gmail.com>
2021-07-25 17:43:40 +0300

    New version

--- source/app_go.go
+++ source/app_go.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello!\nThis is Go App!\nMade with 💖 by Antosha")
+		c.String(http.StatusOK, "Wake up Neo!\nThis is Go App!\nMade with 💖 by Antosha")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
